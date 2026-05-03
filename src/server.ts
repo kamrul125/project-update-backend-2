36f5a90992067d8d8061a531ba85fe6edb23061d
Md kamruzzaman <kamrul.bubtr@gmail.com>
2026-05-03 15:54:07 +0600

    fix: export app for vercel deployment

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,21 @@
-// src/server.ts
+import { Server } from 'http';
 import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+let server: Server;
+
+async function main() {
+  try {
+    server = app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
+
+// Vercel-এর জন্য app-কে অবশ্যই এক্সপোর্ট করতে হবে
+export default app;
